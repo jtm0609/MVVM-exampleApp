42e3196ea78b191c8818854771fde21c4faa79bb
Takmin <jtm0609@naver.com>
2020-11-16 16:19:18 +0900

    MVVM패턴 적용예제

---
+++ app/src/main/java/com/jtmcompany/Model/UserProfieDatabase.java
@@ -0,0 +1,10 @@
+package com.jtmcompany.Model;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+//데이터베이스 이용을 위한 DAO 객체 획득 함수를 제공하는 클래스
+@Database(entities = {UserProfile.class},version = 1)
+public abstract class UserProfieDatabase extends RoomDatabase {
+    public abstract UserProfileDao getUserProfileDao();
+}
